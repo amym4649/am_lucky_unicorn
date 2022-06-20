1006442ca7d9c4c35eed0cab2e177f5142f06971
amym4649 <107890791+amym4649@users.noreply.github.com>
2022-06-21 09:25:43 +1200

    Skeleton of the base code

---
+++ 00_base_v0.py
@@ -0,0 +1,14 @@
+"""
+Base component for the game 'lucky_unicorn'
+v0 - skeleton script
+
+author - Amy Macpherson
+CC AM 2022
+"""
+
+# import libraries below this line **********
+
+# all functions below this line ********
+
+# main routine below this line *********
+print("hello")
