a4fd3a08ace2dbf864971b6e5096b56ec830773d
amym4649 <107890791+amym4649@users.noreply.github.com>
2022-07-05 09:03:21 +1200

    Add files via upload

--- 00_base_v0.py
+++ 00_base_v0.py
@@ -8,7 +8,71 @@ CC AM 2022
 
 # import libraries below this line **********
 
+
 # all functions below this line ********
+def yes_no(question):
+    valid = False
+    while not valid:
+        response = input(question).lower()
+
+        if response == "yes" or response == "y":
+            response = "yes"
+            return response
+
+        # If they say no, output 'display instructions'
+        elif response == "no" or response == "n":
+            response = "no"
+            return response
+
+        else:
+            print("Please input either 'yes' or 'no'.")
+
+
+def instructions():
+    print("* * * How to play * * *")
+    print()
+    print("These are the rules")
+    print()
+    return ""
+
+
+def num_checker(question, low, high):
+
+    error = "Please enter a whole number between 1 and 10\n"
+
+    valid = False
+    while not valid:
+        try:
+            # ask the question
+            response = int(input(question))
+
+            if low < response <= high:
+                return response
+
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
+
 
 # main routine below this line *********
-print("hello")
+played_before = yes_no("Have you played the game before? ")
+
+print()
+
+print("You chose {}".format(played_before))
+
+print()
+
+if played_before == "no":
+    instructions()
+
+# Ask how much they want to play with
+response = num_checker("How much do you want to play with?", 0, 10)
+
+print()
+
+print("You are playing with ${}, continue with the game.\n".format(response))
+
+
